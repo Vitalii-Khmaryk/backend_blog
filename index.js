18fda768917937af11375e39cc2ff2e8dc28e5ce
Vitalii-Khmaryk <vitalikhmaryks@gmail.com>
2023-07-11 22:36:47 +0300

    new backend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Post = require("./models/post");
 require('dotenv').config();
 const app = express();
 
-const port = 3000;
+const port =process.env.PORT || 3000;
 
 app.use(passport.initialize());
 app.use(passport.session());
